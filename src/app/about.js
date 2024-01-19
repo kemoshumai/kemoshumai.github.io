79c32733f0005dc6a1fe65b83ab0536c026e73f6
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2024-01-19 23:12:55 +0900

    説明文を挿入

--- src/app/about.js
+++ src/app/about.js
@@ -4,9 +4,15 @@ export default function About() {
   return (
     <div className={styles.main}>
       <div className={styles.section_edge}></div>
-      <h1>About</h1>
+      <div className={styles.header}>
+        <p>ハイラル平原に巣食う</p>
+        <h1>けもシューマイ</h1>
+      </div>
       <img className={styles.twitter_icon} src="kemoshumai_icon_twitter.jpg" alt="けもシューマイ" />
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, ipsam tempora ullam dolorum dignissimos cumque amet ad necessitatibus fugiat enim commodi? Tenetur rerum quod, repellat aliquid officiis impedit? Cupiditate, suscipit.</p>
+      <p>昔から何かを作ることが好きな人間。</p>
+      <p>HSPから始まり、JavaScript/HTML/Python/Unity/Arduino/Rust/作曲/Blenderなどをやっています。</p>
+      <p>AdobeのサブスクとJavaが苦手。</p>
+      <p>いつかつよつよエンジニアになってろくろを回したい。</p>
     </div>
   )
 }
