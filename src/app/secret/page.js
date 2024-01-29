615ef78d031b7334d9a79466e843e80c90502d53
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2024-01-29 19:00:54 +0900

    古のネットっぽいスタイルを適用

--- src/app/secret/page.js
+++ src/app/secret/page.js
@@ -3,6 +3,9 @@ import styles from "./secret.module.css"
 export default function Secret() {
   return (
     <div className={styles.main}>
+      <h1>秘密のページへようこそ</h1>
+      <p>よくここを見つけましたね！</p>
+      <marquee>あなたは1000000000憶人目のお客様ですwww</marquee>
     </div>
   )
 }
